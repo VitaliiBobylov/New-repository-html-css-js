4b7ed8f1c0cc2a52a3bb7c6735631deca48b59fa
VitaliiBobylov <bobylevvitalyanatolievich@gmail.com>
2025-06-01 09:09:16 +0300

    js

--- js/start.js
+++ js/start.js
@@ -212,19 +212,264 @@
 // const values = apartment;
 
 
-function countTotalSalary(salaries) {
-  let totalSalary = 0;
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
 
-  const salaryValues = Object.values(salaries);
-  for (const salary of salaryValues) {
-    totalSalary += salary;
-  }
+//   const salaryValues = Object.values(salaries);
+//   for (const salary of salaryValues) {
+//     totalSalary += salary;
+//   }
 
-  return totalSalary;
-}
+//   return totalSalary;
+// }
 
 
 
-console.log(countTotalSalary({}));
-console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
-console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+// console.log(countTotalSalary({}));
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+
+
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (const color of colors) {
+//   hexColors.push(color.hex); 
+//   rgbColors.push(color.rgb);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+
+// const product = products.find(item => item.name === productName);
+//   return product ? product.price : null;
+// }
+
+
+
+// console.log(getProductPrice("Radar"));
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+// const values = [];
+//   for (const product of products) {
+//     if (product.hasOwnProperty(propName)) {
+//       values.push(product[propName]);
+//     }
+//   }
+//   return values;
+    
+//   }
+
+
+
+// console.log(getAllPropValues("category"));
+
+
+
+// const atTheOldToad = {
+//     potions: [],
+
+//     getPotions() {
+//         return "List of all available potions";
+//     },
+
+//     addPotion(potionName) {
+//     return `Adding ${potionName}`;
+    
+// }
+// };
+// console.log(atTheOldToad.getPotions());
+
+// console.log(atTheOldToad.addPotion("Invisibility"));
+// console.log(atTheOldToad.addPotion("Power potion"));
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Stone skin"],
+
+//   getPotions() {
+//     return this.potions;
+//   },
+
+//   addPotion(potionName) {
+//     this.potions.push(potionName);
+//   }
+// };
+
+
+// console.log(atTheOldToad.getPotions()); // ["Speed potion", "Stone skin"]
+// atTheOldToad.addPotion("Invisibility");
+// console.log(atTheOldToad.getPotions()); // ["Speed potion", "Stone skin", "Invisibility"]
+// atTheOldToad.addPotion("Power potion");
+// console.log(atTheOldToad.getPotions());
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     this.potions.push(newPotion);
+//   },
+//   getTotalPrice() {
+    
+//   },
+
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 11111111111111111111111111111111111111111111
+// function isEnoughCapacity(products, containerSize) {
+//   const totalItems = Object.values(products).reduce((sum, quantity) => sum + quantity, 0);
+//   return totalItems <= containerSize;
+// }
+
+// console.log(
+//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+// ); // false
+
+// console.log(
+//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+// ); // false
+
+
+
+// 2222222222222222222222222222222222222222222222222222222
+// function calcAverageCalories(days) {
+//   if (days.length === 0) {
+//     return 0; // Повертаємо 0, якщо масив порожній
+//   }
+
+//   const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
+//   return totalCalories / days.length;
+// }
+
+// console.log(
+//   calcAverageCalories([
+//     { day: "monday", calories: 3010 },
+//     { day: "tuesday", calories: 3200 },
+//     { day: "wednesday", calories: 3120 },
+//     { day: "thursday", calories: 2900 },
+//     { day: "friday", calories: 3450 },
+//     { day: "saturday", calories: 3280 },
+//     { day: "sunday", calories: 3300 }
+//   ])
+// ); // 3180
+
+// console.log(
+//   calcAverageCalories([
+//     { day: "monday", calories: 2040 },
+//     { day: "tuesday", calories: 2270 },
+//     { day: "wednesday", calories: 2420 },
+//     { day: "thursday", calories: 1900 },
+//     { day: "friday", calories: 2370 },
+//     { day: "saturday", calories: 2280 },
+//     { day: "sunday", calories: 2610 }
+//   ])
+// ); // 2270
+
+// console.log(
+//   calcAverageCalories([])
+// ); // 0
+
+
+
+// 333333333333333333333333333333333333
+
+// const profile = {
+//     username: "Jacob",
+//   playTime: 300,
+
+//   changeUsername(newName){
+// this.username = newName;
+//   },
+//   updatePlayTime(hours){
+//     this.playTime = hours;
+//   },
+// getInfo(){
+//     return `${this.newName} has ${this.playTime} active hours!`;
+// }
+// }
+
+
+
+
+// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+// profile.changeUsername("Marco");
+// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+// profile.updatePlayTime(20);
+// console.log(profile.getInfo()); // "Marco has 320 active hours!"
